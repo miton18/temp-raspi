a2f20c2e8e96bb3de6aeb22948106096f097bf95
miton18 <rcd18@hotmail.fr>
2016-09-10 19:57:53 +0200

    each 10s

--- index.js
+++ index.js
@@ -2,23 +2,28 @@
 
 var ds18b20 = require('ds18b20');
 
+var sonde = "";
+
 ds18b20.sensors(function(err, ids) {
   
   if (err) {
       console.error(err);
       return;
   }
+  sonde = ids[0];
 
-  console.log(ids);
+  console.log("Sondes: ");
+  console.log(sonde);
 
-  ids.forEach(function(id) {
-      ds18b20.temperature(id , function(err, value) {
+  setInterval(function(){
+      ds18b20.temperature(sonde , function(err, value) {
 
         if (err) {
           console.error(err);
           return;
         }
-        console.log(value);
+        ts  = new Date().getTime();
+        console.log(ts, value); 
       });
-  })
+  }, 10000);
 });
