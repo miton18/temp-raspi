0ff12ddd70b158eacfabca0b81ce09dd21a00da0
miton18 <rcd18@hotmail.fr>
2016-09-10 19:48:18 +0200

    with temp

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ var ds18b20 = require('ds18b20');
 
 ds18b20.sensors(function(err, ids) {
   
-  console.error(err);
+  if (err) {
+      console.error(err);
+      return;
+  }
 
   console.log(ids);
 
+  ids.forEach(function(id) {
+      ds18b20.temperature(id , function(err, value) {
+
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(value);
+      });
+  })
 });
